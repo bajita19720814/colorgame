348c768a95428eef96e14afc5efd076255c7ac35
bajita19720814 <76199148+bajita19720814@users.noreply.github.com>
2021-01-06 15:41:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@
   const select = ["文字の色", "文字の読み", "背景の色"];
   let correctCount = 0;
   let quizNum = 1;
+  let startTime;
   function shaffle(arr) {
     for (let i = arr.length - 1; i >= 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -57,9 +58,14 @@
           } else {
             coment.textContent = '残念！不正解';
           }
-          score.textContent = `SCORE : ${correctCount} / ${quizNum}`;
-          result.classList.add('show');
-          setTimeout(setNextQuiz, 1000);
+          if (correctCount < 10) {
+            score.style.fontSize = '24px';
+            score.textContent = `SCORE : ${correctCount} / ${quizNum}`;
+            result.classList.add('show');
+            setTimeout(setNextQuiz, 900);
+          } else {
+            getResult();
+          }
         });
       });
     }
@@ -69,11 +75,25 @@
       return;
     }
     isGameStarted = true;
+    correctCount = 0;
+    quizNum = 1;
+    result.classList.remove('show');
     new SetQuiz(colors);
+    startTime = Date.now();
   });
   function setNextQuiz() {
     quizNum++;
     result.classList.remove('show');
     new SetQuiz(colors);
   }  
+  function getResult() {
+    const t = Math.floor((Date.now() - startTime) / 1000);
+    coment.textContent = '10問正解！！';
+    score.style.fontSize = '14px';
+    score.textContent = `タイムは${t}秒だよ。もう一度チャレンジするにはクリックしてね`;
+    result.classList.add('show');
+    setTimeout(() => {
+      isGameStarted = false;
+    }, 50);
+  }
 }
